0e7323275f007bdb7f5ae33d977421bdb72c019f
Berchris Requiao <brneto@icloud.com>
2018-05-10 20:05:04 -0300

    Fix course test (work in progress)

--- src/__tests__/client/components/course/CourseForm.test.js
+++ src/__tests__/client/components/course/CourseForm.test.js
@@ -1,39 +1,33 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import CourseForm from '../../../../client/components/course/CourseForm';
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseForm from '../../../../client/components/course/CourseForm';
 
-// function setup(saving) {
-//   let props = {
-//     course:{},
-//     saving: saving,
-//     error: {},
-//     onSubmit: () => {},
-//     onChange: () => {},
-//   };
+function setup(saving) {
+  let props = {
+    course:{},
+    saving: saving,
+    error: {},
+    onSubmit: () => {},
+    onChange: () => {},
+  };
 
-//   return shallow(<CourseForm {...props} />);
-// }
+  return shallow(<CourseForm {...props} />);
+}
 
-// describe('Test CourseForm component', () => {
-//   it('renders form and h1', () => {
-//     const wrapper = setup(false);
-//     expect(wrapper.find('form').length).toBe(1);
-//     expect(wrapper.find('h1')).toHaveText('Manage Course');
-//   });
+describe('Test CourseForm component', () => {
+  it('renders form and h1', () => {
+    const wrapper = setup(false);
+    expect(wrapper.find('form').length).toBe(1);
+    expect(wrapper.find('h1')).toHaveText('Manage Course');
+  });
 
-//   it('save button is labeled "Save" when not saving', () => {
-//     const wrapper = setup(false);
-//     expect(wrapper.find('button')).toHaveText('Save');
-//   });
+  it('save button is labeled "Save" when not saving', () => {
+    const wrapper = setup(false);
+    expect(wrapper.find('button')).toHaveText('Save');
+  });
 
-//   it('save button is labeled "Saving..." when saving', () => {
-//     const wrapper = setup(true);
-//     expect(wrapper.find('button')).toHaveText('Saving...');
-//   });
-// });
-
-describe('Manage Course Page', () => {
-  it('sets error message when trying to save empty title', () => {
-    expect(true).toBe(true);
+  it('save button is labeled "Saving..." when saving', () => {
+    const wrapper = setup(true);
+    expect(wrapper.find('button')).toHaveText('Saving...');
   });
 });
--- src/__tests__/client/components/course/ManageCoursePage.test.js
+++ src/__tests__/client/components/course/ManageCoursePage.test.js
@@ -1,31 +1,25 @@
-// import React from 'react';
-// import { mount } from 'enzyme';
-// import ManageCoursesPage from '../../../../client/components/course/ManageCoursePage';
+import React from 'react';
+import { mount } from 'enzyme';
+import ManageCoursePage from '../../../../client/components/course/ManageCoursePage';
 
-// function setup() {
-//   const props = {
-//     course: { id: '', watchHref: '', title: '', author: '', length: '', category: '' },
-//     authors: [],
-//     actions: { saveCourse: () => Promise.resolve() },
-//     goToRoute: () => '/courses',
-//     saving: false,
-//   };
+function setup() {
+  const props = {
+    course: { id: '', watchHref: '', title: '', author: '', length: '', category: '' },
+    authors: [],
+    actions: { saveCourse: () => Promise.resolve() },
+    goToRoute: () => '/courses',
+    saving: false,
+  };
 
-//   return mount(<ManageCoursesPage.WrappedComponent {...props} />);
-// }
-
-// describe('Manage Course Page', () => {
-//   it('sets error message when trying to save empty title', () => {
-//     const wrapper = setup();
-//     const saveButton = wrapper.find('button');
-//     saveButton.simulate('click');
-//     expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
-//   });
-// });
-
+  return mount(<ManageCoursePage.WrappedComponent {...props} />);
+}
 
 describe('Manage Course Page', () => {
   it('sets error message when trying to save empty title', () => {
-    expect(true).toBe(true);
+    const wrapper = setup();
+    const saveButton = wrapper.find('button');
+    saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
   });
 });
+

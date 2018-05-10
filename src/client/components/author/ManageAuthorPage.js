a27d756f34d327f1f71c9342cde16e9042ff967a
Berchris Requiao <brneto@gmail.com>
2018-05-10 13:13:42 -0300

    Remove form validation

--- src/client/components/author/ManageAuthorPage.js
+++ src/client/components/author/ManageAuthorPage.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { isValid } from 'redux-form/immutable';
 import * as authorThunks from '../../redux/thunks/authorThunks';
 import { authorByIdSelector } from '../../redux/selectors/authorSelectors';
 import AuthorForm from './AuthorForm';
 
 const mapStateToProps = state => ({
   author: authorByIdSelector(state),
-  valid: isValid('author')(state),
   saving: state.get('ajaxCallsInProgress') > 0
 });
 const mapDispatchToProps = dispatch => ({
@@ -20,17 +18,14 @@ const mapDispatchToProps = dispatch => ({
 class ManageAuthorPage extends Component {
   static propTypes = {
     author: PropTypes.object.isRequired,
-    valid: PropTypes.bool.isRequired,
-    saving: PropTypes.bool.isRequired,
     actions: PropTypes.object.isRequired,
+    saving: PropTypes.bool.isRequired,
   };
 
   handleSubmit = values => {
-    const { actions, valid } = this.props;
+    const { actions } = this.props;
 
-    if(valid) {
-      actions.saveAuthor(values.toJS());
-    }
+    actions.saveAuthor(values.toJS());
   };
 
   render() {

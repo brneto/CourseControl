e9ebb85d13c8ad642b44cb5584720930a7e40ef5
Berchris Requiao <brneto@icloud.com>
2018-05-12 12:22:14 -0300

    Fix CourseForm test to consider redux form

--- src/__tests__/client/components/course/CourseForm.test.js
+++ src/__tests__/client/components/course/CourseForm.test.js
@@ -1,42 +1,57 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import CourseForm from '../../../../client/components/course/CourseForm';
+import React from 'react';
+import { render, shallow, mount } from 'enzyme';
+import { createStore } from 'redux';
+import { fromJS } from 'immutable';
+import { routerReducer as router } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { combineReducers } from 'redux-immutable';
+import { reducer as form } from 'redux-form/immutable';
+import { startSubmit, stopSubmit, getFormSyncErrors } from 'redux-form/immutable';
+import authors from '../../../../client/redux/reducers/authorReducer';
+import CourseForm from '../../../../client/components/course/CourseForm';
 
-// function setup(saving) {
-//   let props = {
-//     course:{},
-//     saving: saving,
-//     error: {},
-//     onSubmit: () => {},
-//     onChange: () => {},
-//   };
+function makeStore(initial = {}, logger) {
+  const reducers = { authors, router, form };
+  const preloadedState = fromJS({ form: initial });
+  if (logger) {
+    reducers.logger = logger;
+  }
+  return createStore(combineReducers(reducers), preloadedState);
+}
 
-//   return shallow(<CourseForm {...props} />);
-// }
+const logger = jest.fn((state = {}) => state);
+const store = makeStore({}, logger);
+const getWrapper = store => mount(
+  <Provider store={store}>
+    <CourseForm />
+  </Provider>
+);
+const wrapper = getWrapper(store);
+const formName = 'course';
 
-// describe('Test CourseForm component', () => {
-//   it('renders form and h1', () => {
-//     const wrapper = setup(false);
-//     expect(wrapper.find('form').length).toBe(1);
-//     expect(wrapper.find('h1')).toHaveText('Manage Course');
-//   });
+describe('CourseForm', () => {
+  it('renders form and h1', () => {
+    expect(wrapper.find('form').length).toBe(1);
+    expect(wrapper.find('h1')).toHaveText('Manage Course');
+  });
 
-//   it('save button is labeled "Save" when not saving', () => {
-//     const wrapper = setup(false);
-//     expect(wrapper.find('button')).toHaveText('Save');
-//   });
+  it('save button is labeled "Saving..." when saving', () => {
+    store.dispatch(startSubmit(formName));
 
-//   it('save button is labeled "Saving..." when saving', () => {
-//     const wrapper = setup(true);
-//     expect(wrapper.find('button')).toHaveText('Saving...');
-//   });
-// });
+    expect(wrapper.find('button.btn-primary')).toHaveText('Saving...');
+  });
 
+  it('save button is labeled "Save" when not saving', () => {
+    store.dispatch(stopSubmit(formName));
 
-// TODO:
-// Refact this test to considere this new redux-form component
-describe('Test CourseForm component', () => {
-  it('Remember to make a real test', () => {
-    expect(true).toBe(true);
+    expect(wrapper.find('button.btn-primary')).toHaveText('Save');
+  });
+
+  it('sets error message when trying to save empty title', () => {
+    const saveButton = wrapper.find('button.btn-primary');
+    saveButton.simulate('click');
+    const errors = getFormSyncErrors(formName)(store.getState());
+
+    expect(errors.title).toBe('Required');
   });
 });

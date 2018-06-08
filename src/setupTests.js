2f03313d7e59e1a7eb4a2b1372f10a1ce7fd73f4
Berchris Requiao <brneto@gmail.com>
2018-06-08 14:12:08 -0300

    Add babel polyfill into jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill';
 import 'jest-enzyme'; // Enzyme custom assertions: https://github.com/FormidableLabs/enzyme-matchers
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
